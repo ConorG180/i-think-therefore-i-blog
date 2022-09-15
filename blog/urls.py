7a348553637b3cb08dd77e95c915a25123638f75
Conor <conor.ganly@yahoo.com>
2022-09-15 13:43:31 +0000

    Install and connect allauth to make login and registration

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,3 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
-
-# from . import views
-# from django.urls import path
-
-# urlpatterns = [
-#     path('', views.PostList.as_view(), name='home'),
-#     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-# ]
